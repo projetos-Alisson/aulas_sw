222c4709b951a68054708cb5364372e2ebf597ee
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2022-03-03 08:52:41 -0300

    exemplos de operações realizadas em aula

---
+++ aula_03_03_2022/ex_02.php
@@ -0,0 +1,25 @@
+<?php
+
+$soma = ($valor1 = 4) + 5; //$soma é igual a 9 agora e $valor1 foi configurado como 4.
+
+$valor2 = 20;
+$valor3 = 30;
+
+$soma += 1; //$soma fica com 10
+$soma += $valor2; //$soma fica com 10+20=30
+$soma *= $valor3; //$soma fica com 30*30= 900
+$soma %= 100;
+
+$saudacao = "Bom ";
+$saudacao .= "Dia "; //configura $saudacao para "Bom Dia", como em $saudacao = $saudacao . "Dia!";
+
+echo $saudacao . "Hoje vai ter " . $soma . "% tristeza";
+
+
+
+
+
+
+
+
+?>
---
+++ aula_03_03_2022/ex_03.php
@@ -0,0 +1,19 @@
+<?php
+
+//OPERADORESD DE COMPARAÇÃO
+
+$a = "10";
+$b = 10;
+$soma = $a + $b;
+
+echo "Usando == :". ($a == $b);
+echo "<br>";
+echo "Usando === :". ($a === $b);
+echo "<br>";
+echo "Somando a + b => ". $soma;
+echo "<br>";
+var_dump ($a);
+echo "<br>";
+var_dump($b);
+
+?>
