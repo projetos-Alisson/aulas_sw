a3bad18a0f2ca9e65e62f091319849d134657370
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2022-03-10 09:10:09 -0300

    Add files via upload

---
+++ exercicio/process.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+//declaração de variáveis
+$nota1 = $_POST['nota1'];
+$nota2 = $_POST['nota2'];
+$nota3 = $_POST['nota3'];
+
+$media = ($nota1 + $nota2 + $nota3)/3;
+
+echo "<h1>A média de $num1 + $num2 + $num3 será igual: $media/h1>";
+
+?>
+
+</body>
+</html>
