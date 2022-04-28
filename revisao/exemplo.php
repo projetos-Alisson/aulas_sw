4db2f5a0a83039459f9462a938868ce316cd21fa
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2022-04-28 09:09:21 -0300

    Add files via upload

---
+++ revisao/exemplo.php
@@ -0,0 +1,15 @@
+<?php 
+   $nomes = ["Alisson", "Soares", "Pais"];
+
+   
+   for ($indice = 0; $indice <=2; $indice++){
+     // echo "$nomes[$indice] <br>";
+
+
+     //PARA MOSTRAR A ORDEM DOS NOMES
+     echo ($indice+1) . " º Nome: $nomes[$indice] <br>";
+
+
+   }
+
+?>
---
+++ revisao/while.php
@@ -0,0 +1,24 @@
+<?php
+
+$cont= 0;
+$numero;
+
+while ($cont != 100){
+    echo "EU VOU APRENDER PHP $cont<br>";
+    $cont++;
+}
+
+do{
+    echo "O VALOR ATUAL DO CONTADOR É $numero <br>";
+    $numero++;
+}
+
+while($numero < 15);
+
+echo "Contagem Progressiva <br> <br>";
+for ($cont= 0; $cont> 10; $cont++){
+     echo "A variável \$cont vale $cont <br>";
+
+}
+echo "<br> Contagem Regressiva <br> <br>"
+?>
