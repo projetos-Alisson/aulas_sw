eb0e6a85754b73018f21e95d375ffd2ab51f736b
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2022-05-19 09:10:21 -0300

    Add files via upload

---
+++ EXE_VETOR/EXE_02.php
@@ -0,0 +1,10 @@
+<?php
+//GERAR NUMEROS ALEATÓRIOS
+    $numero = rand(1,10);
+    $outro = $numero/10; //PARA OS NÚMEROS SEREM DO TIPO FLOAT
+    //echo $numero; 
+    echo $outro;
+
+
+
+?>
---
+++ EXE_VETOR/EX_01.php
@@ -0,0 +1,17 @@
+<?php
+
+$meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",  
+          "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+
+
+for($cont=0; $cont<12; $cont++){
+
+  
+}
+echo "O mês escolhido foi: $meses[2]";
+
+
+
+
+?>
