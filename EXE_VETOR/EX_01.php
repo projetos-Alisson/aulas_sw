d130c7911ba9130042e43614df1714f7fa67b215
Alisson Soares <79875609+projetos-Alisson@users.noreply.github.com>
2022-05-19 20:02:06 -0300

    Update EX_01.php

--- EXE_VETOR/EX_01.php
+++ EXE_VETOR/EX_01.php
@@ -2,16 +2,66 @@
 
 $meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",  
           "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+       
+          
+     
+            $escolha = 30;
+            switch ($escolha) {
+                case 1:
+                    
+                    echo $meses[0];
+                    break;
+                case 2:
+                    
+                    echo $meses[1];
+                    break;
+                case 3:
+                    
+                    echo $meses[2];
+                    break;
+                case 4:
+                    
+                    echo $meses[3];
+                    break;
+                case 5:
+                    
+                    echo $meses[4];
+                    break;
+                case 6:
+                    
+                    echo $meses[5];
+                    break;
+                case 7:
+                    
+                    echo $meses[6];
+                    break;
+                case 8:
+                    
+                    echo $meses[7];
+                    break;
+                case 9:
+                    
+                    echo $meses[8];
+                    break;
+                case 10:
+                    
+                    echo $meses[9];
+                    break;
+                case 11:
+                    
+                    echo $meses[10];
+                    break;
+                case 12:
+                    
+                    echo $meses[11];
+                    break;
+                default:
+                    
+                    echo "Digite um número de 1 a 12!";
+                    break;
+            }
 
 
-
-for($cont=0; $cont<12; $cont++){
-
-  
-}
-echo "O mês escolhido foi: $meses[2]";
-
-
-
-
+          
+         
 ?>
